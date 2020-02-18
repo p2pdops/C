12b04df6e92dc6f1191825ae719037f8b89ebe8d
pavan <p2pdops@gmail.com>
2020-02-18 06:14:40 +0530

    Fixed Week 5 programs

--- 19131A1204/week5/week.5.4.c
+++ 19131A1204/week5/week.5.4.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-#include <math.h>
+long fun(int size)
+{
+    long result = 1;
 
+    for(int i = 1 ; i <= size ; i++ ) result = result*i;
+
+    return result;
+}
 int main()
 {
-	// TODO: THATS A TODO (usage of unconditional staements) 
-	return 0;
+    int i, j, size;
+    printf("Enter size of triangle: ");
+    scanf("%d",&size);
+    for ( i = 0 ; i < size ; i++ )
+    {
+        for ( j = 0 ; j < ( size - i - 1 ) ; j++ )
+            printf(" ");
+        for( j = 0 ; j <= i ; j++ )
+            printf("%ld ",fun(i)/(fun(j)*fun(i-j)));
+
+        printf("\n");
+    }
+    return 0;
 }

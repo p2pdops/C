5be08b25b0f95c9b57028e3dd895a896b4d5f23d
pavan <p2pdops@gmail.com>
2020-02-28 07:29:24 +0530

    Update week.4.4.c

--- 19131A1204/week4/week.4.4.c
+++ 19131A1204/week4/week.4.4.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
+void main()
 {
-	// incomplete problem statement (UNCONDITIONAL STATEMENTS)
-	return 0;
+	int a, b, c;
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	int i = 0;
+
+	while(1) {
+
+		i++;
+
+		if(i == c) goto label1;
+		
+		if (i == b) continue;
+		
+		if (i == a) break;
+
+		printf("%d ", i);
+	}
+
+	printf("\n");
+
+	label1 : printf("You r here due to goto : at %d", c);
+
+	printf("\n");
+
 }
+

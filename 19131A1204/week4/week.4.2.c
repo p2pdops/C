d0cd995560d07fb01b538fa423e4572c7bbce2b5
Surya Chandra Pavan Adurthi <p2pdops@gmail.com>
2020-02-28 01:14:16 -0800

    Update week.4.2.c

--- 19131A1204/week4/week.4.2.c
+++ 19131A1204/week4/week.4.2.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
-	
-	int num, i, count;
+	int num, count;
 
 	printf("Enter your number : ");
 
@@ -11,17 +10,25 @@ int main()
 
 	count = 0;
 
-	for (i = 1; i < num + 1; i++) {
+	if( num > 0) {
+
+	printf("The Factors are \n");
+
+	for (int i = 1; i < num + 1; i++) {
 		if (num % i == 0) {
-			printf("%d is factor of %d\n", i, num);	count += 1;
+			printf("%d ", i, num);	count += 1;
 		} 
 	}
 
-	if (count == 2)
-	 printf("%d is a prime number.\n", num);
-	else
-	 printf("%d has %d factors.\n", num, count);
-
 	printf("\n");
-	return 0;
+	
+	if (count == 2) printf("%d is a prime number.\n", num);
+	else  printf("%d is not a prime number.\n", num);
+	
+	} else {
+		printf("Invalid input number!!!");
+	}
+	
+	printf("\n");
+
 }

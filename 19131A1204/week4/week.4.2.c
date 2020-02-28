018fdc989dcd17a9d649968fcb97ead43999bfba
pavan <p2pdops@gmail.com>
2020-02-28 07:30:00 +0530

    Update week.4.2.c

--- 19131A1204/week4/week.4.2.c
+++ 19131A1204/week4/week.4.2.c
@@ -13,13 +13,15 @@ int main()
 
 	for (i = 1; i < num + 1; i++) {
 		if (num % i == 0) {
-			printf("%d is factor of %d\n", i, num);
-			count += 1;
+			printf("%d is factor of %d\n", i, num);	count += 1;
 		} 
 	}
 
-	if (count == 2) printf("%d is a prime number.\n", num);
-	else printf("%d has %d factors.\n", num, count);
+	if (count == 2)
+	 printf("%d is a prime number.\n", num);
+	else
+	 printf("%d has %d factors.\n", num, count);
+
 	printf("\n");
 	return 0;
 }

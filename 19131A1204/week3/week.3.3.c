769c840459b745cf68e46a134024924024dbd357
Surya Chandra Pavan Adurthi <p2pdops@gmail.com>
2020-03-06 04:33:19 +0530

    Update week.3.3.c

--- 19131A1204/week3/week.3.3.c
+++ 19131A1204/week3/week.3.3.c
@@ -12,29 +12,26 @@ int main()
 
 	det = b*b - 4*a*c;
 
-	if (det == 0)
-	{
+	if (det == 0) {
 		alpha = beta = -b/(2*a);
-        
+		
 	    printf("Nature of roots are real and equal, roots are %.2f & %.2f", alpha, beta);
-	}
-	else if (det > 0)
-	{
+	} else if (det > 0) {
 		alpha = (-b + sqrt(det) )/(2*a);
         beta = (-b - sqrt(det) )/(2*a);
 		printf("Nature of roots are real and distint, roots are %.2f & %.2f", alpha, beta);
 	}
-	else 
-	{
+	else {
 
 	alpha = (-b)/(2*a);
+		
 	beta = (sqrt(-det))/(2*a);
 
 	printf("Nature of roots are imaginary and distint, root1 = (%.2f+%.2fi) and root2 = (%.2f-%.2fi)",	alpha,beta, alpha,beta);	
 	
     }
 	
-
 	printf("\n");
+	
 	return 0;
 }
